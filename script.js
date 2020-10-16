9695974ab64a24917195a91f94e98e69131b480c
DPK <dougkvamme@gmail.com>
2020-10-16 14:03:49 -0500

    updated js with notes and modified a couple functions

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-// Assignment Code
+// I leaned on a stackoverlow to get ideas on how to build the functions for this project, it can be found here - https://stackoverflow.com/questions/59904757/random-password-generator-javascript-not-returning-password/59904910#59904910
+
+// Assignment Code, selects #generate from the HTML page
 let generateBtn = document.querySelector("#generate");
 
-// Special characters for the function created
-const specialCharacters = "!@#$%^&*()";
-const generateButton = document.getElementById('generateBtn')
-generateButton.addEventListener('click', writePassword)
-
-// Write password to the #password input
+// Write password function writes the #password to the input
 function writePassword() {
   let password = generatePassword();
   let passwordText = document.querySelector("#password");
@@ -15,21 +12,23 @@ function writePassword() {
 
 }
 
-// Prompts that come up after you click generate password
+// Add event listener to generate password button
+generateBtn.addEventListener('click', writePassword)
+
+// Function for the prompts that come up and make you choose the length and characters you want
 function generatePassword() {
   let passwordLength = prompt("Please enter the number of characters you want for you new password.  It must be more than 8 but less than 128.");
-
-  let numbers = confirm("Do you want numbers in your password?");
 
   let lowerCases = confirm("Do you want lowercases in your password?");
 
   let upperCases = confirm("Do you want uppercases in your password?");
 
+  let numbers = confirm("Do you want numbers in your password?");
+
   let special = confirm("Do you want special characters in your password?");
 
   // this is a minimum count for numbers, lowerCases, upperCases & specialCharacters
   let minimumCount = 0;
-
 
   // Empty minimums for numbers, lowerCases, upperCases & specialCharacters
 
@@ -38,8 +37,10 @@ function generatePassword() {
   let minimumUpperCases = "";
   let minimumSpecialCharacters = "";
 
+// Special characters for the function created
+let specialCharacters = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
-  // Generator functions**
+  // Generator functions
   let functionArray = {
     getNumbers: function() {
       return String.fromCharCode(Math.floor(Math.random() * 10 + 48));
@@ -86,11 +87,11 @@ function generatePassword() {
   }
 
   // empty string variable for the for loop below
-  var randomPasswordGenerated = "";
+  let randomPasswordGenerated = "";
 
   // loop getting random characters
   for (let i = 0; i < (parseInt(passwordLength) - minimumCount); i++) {
-    var randomNumberPicked = Math.floor(Math.random() * 4);
+    let randomNumberPicked = Math.floor(Math.random() * 4);
 
     randomPasswordGenerated += randomNumberPicked;
 
